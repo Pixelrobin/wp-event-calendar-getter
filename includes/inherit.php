48b6a8e178a7a9925c710b0ce453bcc37a4d7fe9
Michael Savchuk <pixelrobin@hotmail.com>
2018-11-02 12:14:00 -0700

    Restart everything

--- includes/inherit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function wp_event_calendar_getter_allowed_post_types() {
-	if function_exists('wp_event_calendar_allowed_post_types') {
-		return wp_event_calendar_allowed_post_types()
-	} else {
-		return array('events');
-	}
-}
